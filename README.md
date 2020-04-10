cfc27fccf51b7a30265e5528604df73b70659707
zanechen <zhenyang0703@gmail.com>
2020-04-10 11:13:39 +0800

    [utility] add init.sh and publish.sh script for the convenience

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 My blog template
+
+This is just reminder for myself. At first, run `init.sh` to do first time init
+
+If want to publish, run `publish.sh`
---
+++ init.sh
@@ -0,0 +1,6 @@
+# init theme
+git submodule update --init --recursive
+
+# init public folder
+
+git submodule add -f -b master https://github.com/u2633/u2633.github.io.git
