86de21ecbf68fad383a0d3e5759f159499e3b609
zane.chen <zhenyang0703@gmail.com>
2020-04-29 01:29:28 +0800

    [post] update how to fix proguard

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ git submodule update --init --recursive
 
 # init public folder
 
-git submodule add -f -b master https://github.com/u2633/u2633.github.io.git public
+git submodule add -f -b master git@github.com:u2633/u2633.github.io.git public
